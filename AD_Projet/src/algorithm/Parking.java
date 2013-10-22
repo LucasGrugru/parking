5930e0cc0d0e07ab260ee489e1553da23f740b24
lgruber <lgruber@lx157>
2013-10-22 12:21:10 +0200

    Creation serveur de voiture

---
+++ AD_Projet/src/algorithm/Parking.java
@@ -0,0 +1,27 @@
+package algorithm;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+public class Parking {
+
+	public int nbPlace;
+	public ArrayList<Porte> portes;
+	
+	public Parking(int nbPlace) {
+		this.nbPlace = nbPlace;
+		portes = new ArrayList<Porte>();
+		for(int i=0; i<this.nbPlace; i++) {
+			try {
+				this.portes.add(new PorteNaimiTrehel());
+			} catch (Exception e) {
+				System.out.println("[PARKING] Error, can not create door");
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void demandeEntree(int idPorte) throws RemoteException {
+		portes.get(idPorte).demandeEntree();
+	}
+}
