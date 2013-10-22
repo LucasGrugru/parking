3c250e01d59970574a6da4eabf6101ccb10b0a78
lgruber <lgruber@lx157>
2013-10-22 11:19:50 +0200

    Ajout Message serializable

---
+++ AD_Projet/src/algorithm/Message.java
@@ -0,0 +1,18 @@
+package algorithm;
+
+import java.io.Serializable;
+
+public class Message implements Serializable {
+
+	public String message;
+	public int demandeur;
+	
+	public Message(String m, int d) {
+		this.message = m;
+		this.demandeur = d;
+	}
+	
+	public Message(String m) {
+		this.message = m;
+	}
+}
