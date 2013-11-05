0e3b271a863403c0711b527ac76264766da7ff66
luckyboss1 <luckyboss1@pc-lucky>
2013-11-05 10:34:48 +0100

    oui

--- AD_Projet/src/test/CreateRegistry.java
+++ AD_Projet/src/test/CreateRegistry.java
@@ -3,10 +3,14 @@ package test;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 
+import reso.Reso;
+import reso.ResoImpl;
+
 public class CreateRegistry {
 
 	public static void main(String[] args) throws RemoteException{
-		LocateRegistry.createRegistry(1201);
+		
+		LocateRegistry.createRegistry(1204);
 		System.out.println("Registry lancé");
 	}
 }
